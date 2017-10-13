3e079aecf2d6a5bd2c9facb96995c73a8b8cfc75
LukeMitchell-N <lukemitchellnelson@gmail.com>
2017-10-12 23:41:08 -0700

    Added javadoc header

--- IllegalOperationException.java
+++ IllegalOperationException.java
@@ -1,3 +1,7 @@
+/**
+ * Exception signifying that an operation has been entered by the user that is
+ * not compatible with those built into the BlockChain
+ */
 public class IllegalOperationException extends RuntimeException {
     public IllegalOperationException(String msg){
         super(msg);
